45b66f7fbd04364c8f9dbd1d27efe3e23538a017
Julien <julien.excoffier@ecole.ensicaen.fr>
2026-02-02 18:17:11 +0100

    Refactor data loading functions

--- src/data.py
+++ src/data.py
@@ -1,10 +1,5 @@
-from sklearn.preprocessing import normalize
-import torch
-from torch.utils.data import DataLoader, Dataset
-import os
-from PIL import Image
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import kagglehub
 
 
 def get_mnist_dataloaders(batch_size=64, normalize=False):
@@ -59,77 +54,3 @@ def get_cifar10_dataloaders(batch_size=64, normalize=False):
     test_dataloader = DataLoader(cifar10_dataset_test, batch_size=batch_size, shuffle=False)
 
     return dataloader, test_dataloader
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-def add_noise(img, sigma):
-    sigma = sigma / 255.0
-    noise = torch.randn_like(img) * sigma
-    noisy_img = img + noise
-    noisy_img = torch.clamp(noisy_img, 0.0, 1.0)
-    return noisy_img
-
-
-class LandscapeDenoisingDataset(Dataset):
-    def __init__(self, root_dir, transforms, sigma=25):
-        self.root_dir = root_dir
-        self.sigma = sigma
-
-        self.images = [
-            os.path.join(root_dir, f)
-            for f in os.listdir(root_dir)
-            if f.lower().endswith((".jpg", ".png", ".jpeg"))
-        ]
-
-        self.transforms = transforms
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        img = Image.open(self.images[idx]).convert("RGB")
-        img = self.transforms(img)
-
-        noisy = add_noise(img, self.sigma)
-
-        return noisy, img
-
-
-def get_landscape_dataloaders(batch_size=256, normalize=True):
-
-    path = kagglehub.dataset_download("arnaud58/landscape-pictures")
-
-    print("Path to dataset files:", path)
-
-    if normalize:
-        transforms = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
-    else:
-        transforms = transforms.ToTensor()
-
-    dataset = LandscapeDenoisingDataset(root_dir=path, transforms=transforms, sigma=25)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-
-    return dataloader
